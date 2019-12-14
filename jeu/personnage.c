a248f8e66f303f50a6ea2d09f8f4a1f1b4cd2e2c
Alpaw <alpaslan_09@hotmail.fr>
2019-12-15 00:27:29 +0100

    Nettoyage Git
    Nettoyage code

--- jeu/personnage.c
+++ jeu/personnage.c
@@ -1,5 +1,18 @@
 #include "personnage.h"
 
+void remplir_rect(SDL_Rect*A,SDL_Rect*B,SDL_Rect*C,SDL_Rect*D,SDL_Rect*AA,SDL_Rect*BB,SDL_Rect*CC,SDL_Rect*DD){
+    A->x=0;A->y=0;A->h=TLuffy;A->w=TLuffy;
+    AA->x=100;AA->y=50;AA->h=TLuffy;AA->w=TLuffy;
+
+    B->x=0;B->y=0;B->h=TLuffy;B->w=TLuffy;
+    BB->x=0;BB->y=345;BB->h=TLuffy;BB->w=TLuffy;
+
+    C->x=0;C->y=0;C->h=TLuffy;C->w=TLuffy;
+    CC->x=107;CC->y=245;CC->h=TLuffy;CC->w=TLuffy;
+
+    D->x=0;D->y=0;D->h=TLuffy;D->w=TLuffy;
+    DD->x=310;DD->y=420;DD->h=TLuffy;DD->w=TLuffy;
+}
 
 
 void detection_porte(char** map,SDL_Rect* luffy,luffy_t* personnage,int* n,int* m){
